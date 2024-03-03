43ced23bf0a6ca0db7ff82be743c8a8965a81bc2
AmalVerma <abverma_b23@it.vjti.ac.in>
2024-03-03 12:12:22 +0530

    download typescript correction

--- powerpoint-generator-client/app/download/page.tsx
+++ powerpoint-generator-client/app/download/page.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import PowerPoint from '../components/PowerPoint'
 
 const download = () => {
-
   return (
     <div className='flex flex-col justify-center items-center'>
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mt-10">Download PowerPoint Here</h1>
         <button className="btn btn-secondary btn-lg mt-5" onClick={() => {
-            PowerPoint(JSON.parse(localStorage.getItem("RawText")));
+            const rawText = localStorage.getItem("RawText");
+            if (rawText) {
+              PowerPoint(JSON.parse(rawText));
+            }
         }}>Download</button>
     </div>
   )
